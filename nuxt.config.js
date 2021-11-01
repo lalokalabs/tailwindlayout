591901739efbc4b87b763b0b97ee78401630e505
Muhammad Fathur Rahman <fathur@xoxzo.com>
2021-11-01 09:47:19 +0800

    feat: update route base

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/tailwindlayout/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'tailwindlayout',
