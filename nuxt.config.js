3e231345d9271d3773d2902e79c0dcad43b44588
Muhammad Fathur Rahman <fathur@xoxzo.com>
2021-11-15 22:24:55 +0800

    feat: add google site verification

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,11 +19,12 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'google-site-verification', content: process.env.GOOGLE_SITE_VERIFICATION_ID }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
