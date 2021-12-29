ccabf5b332fb166a0ca49859abfa69c95e93ab1c
Muhammad Fathur Rahman <fathur@xoxzo.com>
2021-12-29 10:07:17 +0800

    feat: update top banner

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,18 +1,18 @@
 <template>
   <header class="border-b shadow-sm">
-    <div
-      class="
-        max-w-7xl
-        mx-auto
-        flex
-        items-center
-        justify-between
-        px-4
-        py-4
-      "
-    >
+    <div class="bg-indigo-50 bg-opacity-50 border-b border-indigo-50 py-2 text-center">
+      <a class="text-sm text-indigo-500 font-medium hover:underline" href="https://getotp.dev"
+        >Need OTP verification for your app? Forget custom code and try GetOTP
+        now!</a
+      >
+    </div>
+    <div class="max-w-7xl mx-auto flex items-center justify-between px-4 py-4">
       <NuxtLink class="font-bold flex-none" to="/">
-        <img src="/lalokalayout-logo.png" class="h-[40px]" alt="LaLoka Layouts Logo">
+        <img
+          src="/lalokalayout-logo.png"
+          class="h-[40px]"
+          alt="LaLoka Layouts Logo"
+        />
       </NuxtLink>
 
       <div class="w-full mx-4 sm:mx-8">
@@ -43,13 +43,27 @@
       <div class="hidden sm:block min-w-[82px]">
         <ul class="flex space-x-6">
           <li>
-            <a href="https://www.facebook.com/layoutsfortailwind" target="_blank">
-              <img src="/facebook-logo.png" class="w-[29px] opacity-60 hover:opacity-80" alt="Facebook Logo">
+            <a
+              href="https://www.facebook.com/layoutsfortailwind"
+              target="_blank"
+            >
+              <img
+                src="/facebook-logo.png"
+                class="w-[29px] opacity-60 hover:opacity-80"
+                alt="Facebook Logo"
+              />
             </a>
           </li>
           <li>
-            <a href="https://github.com/lalokalabs/tailwindlayout" target="_blank">
-              <img src="/github-logo.png" class="w-[29px] opacity-60 hover:opacity-80"  alt="GitHub Logo">
+            <a
+              href="https://github.com/lalokalabs/tailwindlayout"
+              target="_blank"
+            >
+              <img
+                src="/github-logo.png"
+                class="w-[29px] opacity-60 hover:opacity-80"
+                alt="GitHub Logo"
+              />
             </a>
           </li>
         </ul>
